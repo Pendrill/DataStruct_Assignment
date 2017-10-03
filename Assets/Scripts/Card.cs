01070157685d1135f07d612848ad688f0f76390e
Aleksei Waddington <amw694@nyu.edu>
2017-10-02 20:37:52 -0400

    The system now creates a reference to all the cards on startup. cards have the right names and values.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour {
+public class Card {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private string p_name;
+    private int p_value;
+    private bool p_visible;
+    private bool p_inDeck;
+
+    public Card()
+    {
+        p_name = "";
+        p_value = 0;
+        p_visible = false;
+        p_inDeck = false;
+        
+    }
+    public Card(int value, int suit): this(){
+        
+        if(value <= 10)
+        {
+            //Debug.Log(suit);
+            p_name = (value) + "_" + GameManager.instance.cardSuits[suit];
+            p_value = value;
+        }else if(value > 10){
+            //Debug.Log(value);
+            p_name = GameManager.instance.cardFace[value]+ "_" + GameManager.instance.cardSuits[suit];
+            p_value = 10;
+        }
+
+    }
+
+    public string name
+    {
+        get
+        {
+            return p_name;
+        }
+    }
+
+    public int value
+    {
+        get
+        {
+            return p_value;
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    public static GameManager instance = new GameManager();
+    string[] suit = { "heart", "diamond", "spade", "clover" };
+    string[] face = { "Jack", "Queen", "King" };
+    int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14 };
+    Card[] cardReference = new Card[52];
 
-	// Use this for initialization
-	void Start () {
-		
+    public Dictionary<int, string> cardSuits = new Dictionary<int, string>();
+    public Dictionary<int, string> cardFace = new Dictionary<int, string>();
+    // Use this for initialization
+    void Start () {
+        instance = this;
+        int counter = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            cardSuits.Add(i, suit[i]);
+        }
+        for (int i = 12; i< 15; i++)
+        {
+            //Debug.Log(counter); 
+            cardFace.Add(i, face[counter]);
+            counter += 1;
+            
+        }
+        createAllCards();
+        debugCard();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    /// <summary>
+    /// function used at the start, will instantiate all the necessary card objects.
+    /// </summary>
+    void createAllCards()
+    {
+        for (int i = 0; i < 13; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                cardReference[i + (j * 13)] = new Card(number[i], j);
+            }
+        }
+    }
+
+    void debugCard()
+    {
+        for(int i = 0; i<cardReference.Length; i++)
+        {
+            Debug.Log(cardReference[i].name + "\t" + cardReference[i].value);
+        }
+    }
 }
